24f8f580c79d855496b3e3a5aa079c98f8d69b4c
solal <solal@getlocalmeasure.com>
2015-07-03 11:34:11 +1000

    Initial Commit

---
+++ WidgetsPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WidgetsPrototype
+//
+//  Created by solal on 3/07/2015.
+//  Copyright (c) 2015 solal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
