f34c3c97ff89f94e21e9000a17fd8c0172c0bb6a
solal <solal.fitoussi@gmail.com>
2015-07-31 16:54:04 +1000

    Setting up the unit tests for the Widget Gateway
    - Adding a local JSON file to be able to test the model.
    - Putting together async testing and checking we get and number of widgets superior to zero.

---
+++ WidgetsPrototypeTests/WidgetsGatewayTestCase.swift
@@ -0,0 +1,33 @@
+//
+//  WidgetsGatewayTestCase.swift
+//  WidgetsPrototype
+//
+//  Created by solal on 24/07/2015.
+//  Copyright (c) 2015 solal. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WidgetsGatewayTestCase: XCTestCase {
+    
+    func setEnv() {
+        let envList = ["test": "file://\(NSBundle.mainBundle().resourcePath!)"]
+        EnvManager.sharedInstance.envConfig(envList, env: "test", userHash: "")        
+    }
+    
+    func testGetWidgetsList() {
+        self.setEnv()
+        
+        let loadedExpectation = expectationWithDescription("Loaded widgets.")
+        WidgetsGateway().fetch { (widgets) -> Void in
+            XCTAssertGreaterThan(widgets.count, 0, "Failed to return widgets.")
+            loadedExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(15, handler: { error in
+            XCTAssertNil(error, "Error loading widgets")
+        })
+    }
+    
+}
